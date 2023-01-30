f8e1ad5a06d772d0f4dea864f35bccbce6e13bb8
LuckyChimp <lukas.hartwich00@gmail.com>
2023-01-30 16:27:37 +0100

    Implement svg elements and symbols as components

--- frontend/src/components/Comment.jsx
+++ frontend/src/components/Comment.jsx
@@ -1,4 +1,9 @@
 import { useState } from "react";
+import { ReactComponent as PlusIcon } from "../assets/images/icon-plus.svg";
+import { ReactComponent as MinusIcon } from "../assets/images/icon-minus.svg";
+import { ReactComponent as ReplyIcon } from "../assets/images/icon-reply.svg";
+import { ReactComponent as DeleteIcon } from "../assets/images/icon-delete.svg";
+import { ReactComponent as EditIcon } from "../assets/images/icon-edit.svg";
 
 const Comment = ({ comment, own, replyingTo, onVoteClick, onDeleteClick, onUpdateClick, onAddReplyClick }) => {
     const [voted, setVoted] = useState(false);
@@ -53,7 +58,7 @@ const Comment = ({ comment, own, replyingTo, onVoteClick, onDeleteClick, onUpdat
                         return onVoteClick(comment.id, +1);
                     }}
                     className="comment-voter-upvote">
-                    +
+                    <PlusIcon className="comment-voter-upvote-icon"/>
                 </button>
                 <span className="comment-voter-score">{comment.score}</span>
                 <button
@@ -69,7 +74,7 @@ const Comment = ({ comment, own, replyingTo, onVoteClick, onDeleteClick, onUpdat
                         return onVoteClick(comment.id, -1);
                     }}
                     className="comment-voter-downvote">
-                    -
+                    <MinusIcon className="comment-voter-downvote-icon"/>
                 </button>
             </div>
             <div className="comment-main">
@@ -92,7 +97,7 @@ const Comment = ({ comment, own, replyingTo, onVoteClick, onDeleteClick, onUpdat
                             ? <button
                                 onClick={onAddReplyClick}
                                 className="comment-main-header-reply comment-main-header-action">
-                                <svg width="14" height="13" xmlns="http://www.w3.org/2000/svg" className="comment-main-header-reply-icon comment-main-header-action-icon"><title>reply icon</title><path d="M.227 4.316 5.04.16a.657.657 0 0 1 1.085.497v2.189c4.392.05 7.875.93 7.875 5.093 0 1.68-1.082 3.344-2.279 4.214-.373.272-.905-.07-.767-.51 1.24-3.964-.588-5.017-4.829-5.078v2.404c0 .566-.664.86-1.085.496L.227 5.31a.657.657 0 0 1 0-.993Z" fill="#5357B6" /></svg>
+                                <ReplyIcon className="comment-main-header-reply-icon comment-main-header-action-icon" />
                                 Reply
                             </button>
                             : ''
@@ -101,7 +106,7 @@ const Comment = ({ comment, own, replyingTo, onVoteClick, onDeleteClick, onUpdat
                             ? <button
                                 onClick={() => onDeleteClick(comment.id)}
                                 className="comment-main-header-delete comment-main-header-action">
-                                <svg width="12" height="14" xmlns="http://www.w3.org/2000/svg" className="comment-main-header-delete-icon comment-main-header-action-icon"><title>delete icon</title><path d="M1.167 12.448c0 .854.7 1.552 1.555 1.552h6.222c.856 0 1.556-.698 1.556-1.552V3.5H1.167v8.948Zm10.5-11.281H8.75L7.773 0h-3.88l-.976 1.167H0v1.166h11.667V1.167Z" fill="#ED6368" /></svg>
+                                <DeleteIcon className="comment-main-header-delete-icon comment-main-header-action-icon" />
                                 Delete
                             </button>
                             : ''
@@ -110,7 +115,7 @@ const Comment = ({ comment, own, replyingTo, onVoteClick, onDeleteClick, onUpdat
                             ? <button
                                 onClick={() => setEditMode(!editMode)}
                                 className="comment-main-header-edit comment-main-header-action">
-                                <svg width="14" height="14" xmlns="http://www.w3.org/2000/svg" className="comment-main-header-edit-icon comment-main-header-action-icon"><title>edit icon</title><path d="M13.479 2.872 11.08.474a1.75 1.75 0 0 0-2.327-.06L.879 8.287a1.75 1.75 0 0 0-.5 1.06l-.375 3.648a.875.875 0 0 0 .875.954h.078l3.65-.333c.399-.04.773-.216 1.058-.499l7.875-7.875a1.68 1.68 0 0 0-.061-2.371Zm-2.975 2.923L8.159 3.449 9.865 1.7l2.389 2.39-1.75 1.706Z" fill="#5357B6" /></svg>
+                                <EditIcon className="comment-main-header-edit-icon comment-main-header-action-icon" />
                                 Edit
                             </button>
                             : ''
