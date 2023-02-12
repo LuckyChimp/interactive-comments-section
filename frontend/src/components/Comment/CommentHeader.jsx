65e2c700b88512cab4103a2e145e0590e03e2bcb
LuckyChimp <lukas.hartwich00@gmail.com>
2023-02-12 18:37:38 +0100

    Undo implementation of day.js library due to customization restrictions

--- frontend/src/components/Comment/CommentHeader.jsx
+++ frontend/src/components/Comment/CommentHeader.jsx
@@ -1,8 +1,5 @@
 import { useState, useEffect } from "react";
 
-import DayJs from "dayjs";
-import RelativeTime from "dayjs/plugin/relativeTime";
-
 import { fetchUser } from "../../api";
 
 import LazyAvatar from "../LazyAvatar";
@@ -11,10 +8,6 @@ import { ReactComponent as DeleteIcon } from "../../assets/images/icon-delete.sv
 import { ReactComponent as EditIcon } from "../../assets/images/icon-edit.svg";
 
 const CommentHeader = ({ userID, createdAt, own, onEditClick }) => {
-    // implement dayjs to calculate the elapsed time since the comment was created in seconds, minutes, hours, days, weeks, months or years
-    DayJs().format();
-    DayJs.extend(RelativeTime);
-
     return (
         <div className="comment-main-header">
             <HeaderInfo userID={userID} createdAt={createdAt} own={own} />
@@ -25,6 +18,36 @@ const CommentHeader = ({ userID, createdAt, own, onEditClick }) => {
 
 const HeaderInfo = ({ userID, createdAt, own }) => {
     const [username, setUsername] = useState(null);
+
+    const timeSince = (date) => {
+        const now = Date.now();
+        const diffInMs = Math.abs(now - date);
+        const diff = {
+            "seconds": Math.floor(diffInMs / 1000),
+            "minutes": Math.floor(diffInMs / 1000 / 60),
+            "hours": Math.floor(diffInMs / 1000 / 60 / 60),
+            "days": Math.floor(diffInMs / 1000 / 60 / 60 / 24),
+            "weeks": Math.floor(diffInMs / 1000 / 60 / 60 / 24 / 7),
+            "months": Math.floor(diffInMs / 1000 / 60 / 60 / 24 / (365 / 12)),
+            "years": Math.floor(diffInMs / 1000 / 60 / 60 / 24 / 365)
+        }
+
+        if (diff.years >= 1) {
+            return `${diff.years} year${diff.years > 1 ? 's' : ''} ago`;
+        } else if (diff.months >= 1) {
+            return `${diff.months} month${diff.months > 1 ? 's' : ''} ago`;
+        } else if (diff.weeks >= 1) {
+            return `${diff.weeks} week${diff.weeks > 1 ? 's' : ''} ago`;
+        } else if (diff.days >= 1) {
+            return `${diff.days} day${diff.days > 1 ? 's' : ''} ago`;
+        } else if (diff.hours >= 1) {
+            return `${diff.hours} hour${diff.hours > 1 ? 's' : ''} ago`;
+        } else if (diff.minutes >= 1) {
+            return `${diff.minutes} minute${diff.minutes > 1 ? 's' : ''} ago`;
+        } else if (diff.seconds >= 1) {
+            return `${diff.seconds} second${diff.seconds > 1 ? 's' : ''} ago`;
+        }
+    }
 
     useEffect(() => {
         let active = true;
@@ -38,7 +61,7 @@ const HeaderInfo = ({ userID, createdAt, own }) => {
         return () => {
             active = false;
         }
-    }, []);
+    }, [userID]);
 
     return (
         <div className="comment-main-header-info">
@@ -48,7 +71,7 @@ const HeaderInfo = ({ userID, createdAt, own }) => {
                 ? <span className="comment-owner-tag">you</span>
                 : ''
             }
-            <span className="comment-created">{DayJs(new Date(createdAt)).fromNow()}</span>
+            <span className="comment-created">{timeSince(new Date(createdAt))}</span>
         </div>
     );
 };
