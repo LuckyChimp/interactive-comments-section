546b750e4803b78e2be43c3d85250c798822d564
LuckyChimp <lukas.hartwich00@gmail.com>
2023-02-12 16:26:01 +0100

    Split commentHeader component for better readability

--- frontend/src/components/Comment/CommentHeader.jsx
+++ frontend/src/components/Comment/CommentHeader.jsx
@@ -13,46 +13,58 @@ const CommentHeader = ({ userID, createdAt, own, onEditClick }) => {
 
     return (
         <div className="comment-main-header">
-            <div className="comment-main-header-info">
-                <LazyAvatar userID={userID} width="2rem" height="2rem" className="comment-author-profile-photo" alt="author" />
-                <span className="comment-author">{userID}</span>
-                {own
-                    ? <span className="comment-owner-tag">you</span>
-                    : ''
-                }
-                <span className="comment-created">{DayJs(new Date(createdAt)).fromNow()}</span>
-            </div>
-            <div className="comment-main-header-actions">
-                {!own
-                    ? <button
-                        onClick={() => { }}
-                        className="comment-main-header-reply comment-main-header-action">
-                        <ReplyIcon className="comment-main-header-reply-icon comment-main-header-action-icon" />
-                        Reply
-                    </button>
-                    : ''
-                }
-                {own
-                    ? <button
-                        onClick={() => { }}
-                        className="comment-main-header-delete comment-main-header-action">
-                        <DeleteIcon className="comment-main-header-delete-icon comment-main-header-action-icon" />
-                        Delete
-                    </button>
-                    : ''
-                }
-                {own
-                    ? <button
-                        onClick={() => onEditClick()}
-                        className="comment-main-header-edit comment-main-header-action">
-                        <EditIcon className="comment-main-header-edit-icon comment-main-header-action-icon" />
-                        Edit
-                    </button>
-                    : ''
-                }
-            </div >
-        </div >
+            <HeaderInfo userID={userID} createdAt={createdAt} own={own} />
+            <HeaderActions own={own} onEditClick={() => onEditClick()} />
+        </div>
     );
 }
+
+const HeaderInfo = ({ userID, createdAt, own }) => {
+    return (
+        <div className="comment-main-header-info">
+            <LazyAvatar userID={userID} width="2rem" height="2rem" className="comment-author-profile-photo" alt="author" />
+            <span className="comment-author">{userID}</span>
+            {own
+                ? <span className="comment-owner-tag">you</span>
+                : ''
+            }
+            <span className="comment-created">{DayJs(new Date(createdAt)).fromNow()}</span>
+        </div>
+    );
+};
+
+const HeaderActions = ({ own, onEditClick }) => {
+    return (
+        <div className="comment-main-header-actions">
+            {!own
+                ? <button
+                    onClick={() => { }}
+                    className="comment-main-header-reply comment-main-header-action">
+                    <ReplyIcon className="comment-main-header-reply-icon comment-main-header-action-icon" />
+                    Reply
+                </button>
+                : ''
+            }
+            {own
+                ? <button
+                    onClick={() => { }}
+                    className="comment-main-header-delete comment-main-header-action">
+                    <DeleteIcon className="comment-main-header-delete-icon comment-main-header-action-icon" />
+                    Delete
+                </button>
+                : ''
+            }
+            {own
+                ? <button
+                    onClick={() => onEditClick()}
+                    className="comment-main-header-edit comment-main-header-action">
+                    <EditIcon className="comment-main-header-edit-icon comment-main-header-action-icon" />
+                    Edit
+                </button>
+                : ''
+            }
+        </div>
+    );
+};
 
 export default CommentHeader;
