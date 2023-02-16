d5cf30f8370dd56ec02094aef080e06a9d3a13f8
LuckyChimp <lukas.hartwich00@gmail.com>
2023-02-16 19:59:18 +0100

    Serve frontend if in production mode

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const cors = require('cors');
 const colors = require('colors');
@@ -17,6 +18,15 @@ app.use(cors()); // TODO check if cors implementation is necessery when its depl
 
 app.use('/api/comments', require('./routes/commentRoutes'));
 app.use('/api/users', require('./routes/userRoutes'));
+
+// Serve frontend
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+	app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')));
+} else {
+	app.get('/', (req, res) => res.send('Please set to production'));
+}
 
 app.use(errorHandler);
 
